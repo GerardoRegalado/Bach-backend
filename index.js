7af04a60a9527b6af95ed53a9a6acd953c6fd6bc
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-03-03 20:03:28 -0600

    User template working

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json({extended: true}))
 
 // 3. RUTEO
 app.use("/api/users", require("./routes/users"))
-app.use("/api/pets", require("./routes/pets"))
+
 app.use("/", require("./routes/index"))
 
 
